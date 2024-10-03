b49b6ce708dc2877ab189e826f7deef1a14a919e
mingderwang <mingderwang@me.com>
2024-10-02 19:06:34 -0700

    ok go for vercel

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import {
 } from 'viem/account-abstraction'
 import { privateKeyToAccount } from 'viem/accounts'
 import { sepolia } from 'viem/chains'
-require('dotenv').config();
+import 'dotenv/config';
 console.log(process.env.VITE_BUNDLER_RPC_URL)
 const client = createPublicClient({
   chain: sepolia,
